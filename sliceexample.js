4d0e9c4c18a9448b7c36f254d219c774ae176d84
Raksha <shetty.raksha@gmail.com>
2021-10-17 23:44:17 +0530

    changes to file

--- sliceexample.js
+++ sliceexample.js
@@ -13,3 +13,13 @@ var array = ['books', 'games', 'cup', 'sandwich', 'bag', 'phone', 'cactus']
   // noPlats = ['books', 'games', 'cup', 'sandwich', 'bag', 'phone']
   
   // array will still equal ['books', 'games', 'cup', 'sandwich', 'bag', 'phone', 'cactus']
+
+
+  function viewArgs() {
+    return arguments;
+}
+console.log(viewArgs([3, 5, 1, 2, 2], 2, 3, 5));    
+console.log(viewArgs([2, 3, 2, 3, 2, 3]));   
+console.log(viewArgs(3,2,1,"life the universe and all"));
+console.log(viewArgs("Douglas","Adams"));
+console.log(viewArgs(["hello", "World!", 'and', 'thanks', 'for','all','the', 'fish'], "dolphines", 42));    

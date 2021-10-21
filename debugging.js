b7899095f60eb19d0579d334d0b6a5b9b1654561
Raksha <shetty.raksha@gmail.com>
2021-10-21 19:23:54 +0530

    started debuggingsection

---
+++ debugging.js
@@ -0,0 +1,17 @@
+let a = 5;
+let b = 1;
+a++;
+// Only change code below this line
+console.log(a)
+
+let sumAB = a + b;
+console.log(sumAB);
+
+//checking typeof
+
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// Only change code below this line
+console.log(typeof seven)
+console.log(typeof three)
--- phonenumberregex.js
+++ phonenumberregex.js
@@ -1,46 +1,41 @@
 function telephoneCheck(str) {
     var flag
-         var regex1 = /\d\d\d[-]\d\d\d-\d\d\d/
-         var regex4 = /5555555555/
-        var regex2 = /^1[\s\S][(]\d\d\d[)][\s\S]\d\d\d[\s\S]\d\d\d/
-        var regex5 = /^1[\s\S]\d\d\d[\s\S]\d\d\d[\s\S]\d\d\d/
-        var regex3 = /(555)[\s\S]\d\d\d[\s\S]\d\d\d/
-        var regex6 = /1 555)555-5555/
-        // var regex7 = /1 555)555-5555"/
+                var regex3 = /(555)[\s\S]\d\d\d[\s\S]\d\d\d/
+                 var regex1 = /^\d\d\d[-]\d\d\d-\d\d\d$/
+         var regex4 = /5{10}/
+        var regex2 = /^1{1}[\s\S][(]\d\d\d[)][\s\S]\d\d\d[\s\S]\d\d\d/
+        var regex5 = /^1{1}[\s\S]\d\d\d[\s\S]\d\d\d[\s\S]\d\d\d/
+
+        
         if (regex2.test(str))
         {
             flag = true
-           console.log(regex2.exec(str))
+           console.log("regex2 ",regex2.exec(str))
         }
         else if (regex1.test(str))
         {
             flag = true
-           console.log(regex1.exec(str))
+           console.log("regex1 ",regex1.exec(str))
         }
         else if (regex3.test(str))
         {
             flag = true
-           console.log(regex3.exec(str))
+           console.log("regex3 ",regex3.exec(str))
         }
         else if (regex4.test(str))
         {
             flag = true
-           console.log(regex4.exec(str))
+           console.log("regex4 ",regex4.exec(str))
         }
         else if (regex5.test(str))
         {
             flag = true
-           console.log(regex5.exec(str))
+           console.log("regex5 ",regex5.exec(str))
         }
-        else if (regex6.test(str))
-        {
-            flag = false
-           console.log(regex5.exec(str))
-        }
-        else     flag = false
+               else     flag = false
     
         console.log(flag)
           return flag;
     
     }
-       telephoneCheck("(555-555-5555");
+       telephoneCheck("1 555)555-5555");

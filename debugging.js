5250c9ddfff43d847354df79255c7ae8edecf776
Raksha <shetty.raksha@gmail.com>
2021-10-22 21:50:11 +0530

    did USCode Regex

--- debugging.js
+++ debugging.js
@@ -15,3 +15,75 @@ console.log(seven + three);
 // Only change code below this line
 console.log(typeof seven)
 console.log(typeof three)
+
+//reduce
+
+let myArray = [1, 2, 3];
+let previous=0
+let arraySum = myArray.reduce((previous, current) =>  previous + current);
+console.log(`Sum of array values is: ${arraySum}`);
+
+//quotes
+
+let innerHtml = '\"<p>Click here to <a href=\"#Home\">return home</a></p>;\"'
+console.log(innerHtml);
+
+//function
+function getNine() {
+    let x = 6;
+    let y = 3;
+    return x + y;
+  }
+  
+  let result = getNine();
+  console.log(result);
+
+  //exp
+
+  function raiseToPower(b, e) {
+    return Math.pow(b, e);
+  }
+  
+  let base = 2;
+  let exp = 3;
+  let power = raiseToPower(base,exp);
+  console.log(power);
+
+  //counttofive
+  function countToFive() {
+    let firstFive = "12345";
+    let len = firstFive.length;
+    // Only change code below this line
+    for (let i = 0; i < len; i++) {
+    // Only change code above this line
+      console.log(firstFive[i]);
+    }
+  }
+  
+  countToFive();
+
+  //2d array
+
+  function zeroArray(m, n) {
+    // Creates a 2-D array with m rows and n columns of zeroes
+    let newArray = [];
+    let row = [];
+    for (let i = 0; i < m; i++) {
+      // Adds the m-th row into newArray
+  
+      for (let j = 0; j < n; j++) {
+        // Pushes n zeroes into the current row to create the columns
+        row.push(0);
+      }
+      // Pushes the current row, which now has n zeroes in it, to the array
+      newArray.push(row);
+      row=[]
+    }
+    return newArray;
+  }
+  
+  let matrix = zeroArray(3, 2);
+  console.log(matrix);
+
+  
+  
--- phonenumberregex.js
+++ phonenumberregex.js
@@ -1,13 +1,25 @@
 function telephoneCheck(str) {
     var flag
-                var regex3 = /(555)[\s\S]\d\d\d[\s\S]\d\d\d/
-                 var regex1 = /^\d\d\d[-]\d\d\d-\d\d\d$/
+    var len1, len2
+                var regex3 = /^\(555\)[\d]{3}[\s-]*[\d]{3}/
+                 var regex1 = /^555[-]\d\d\d-\d\d\d/
          var regex4 = /5{10}/
-        var regex2 = /^1{1}[\s\S][(]\d\d\d[)][\s\S]\d\d\d[\s\S]\d\d\d/
-        var regex5 = /^1{1}[\s\S]\d\d\d[\s\S]\d\d\d[\s\S]\d\d\d/
-
+          var regex2 = /^1{1}[\s-]*\d\d\d[\s-]*\d\d\d[\s-]*\(*\d\d\d/
+        var regex5 = /^1{1}[\s-]*\(\d\d\d\)[\s-]*[\d]{3}[\s-]*[\d]{3}/
+        var regex6 = /[(]/
+        var regex7 = /[)]/
+        if(regex6.test(str)){len1= (str.match(regex6).length)
+        console.log(len1)}
+         else len1=0
+       if(regex7.test(str)){var len2= (str.match(regex7).length)
+        console.log(len1)}
+       else len2 = 0
+       if (len1!==len2)
+       {
+         flag = false 
+       }
         
-        if (regex2.test(str))
+       if (regex2.test(str))
         {
             flag = true
            console.log("regex2 ",regex2.exec(str))
@@ -38,4 +50,4 @@ function telephoneCheck(str) {
           return flag;
     
     }
-       telephoneCheck("1 555)555-5555");
+       telephoneCheck("1 555)555-5555") 
